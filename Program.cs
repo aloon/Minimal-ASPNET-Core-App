795cdea5dd0c1812e4c2dbb1575ff19c42202a7a
Alejandro Gonzalez <alejandro.gonzalez@scmspain.com>
2016-09-05 12:10:07 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new WebHostBuilder()
+                .UseKestrel()
+                .Configure(a => a.Run(c => c.Response.WriteAsync("Hello world")))
+                .Build()
+                .Run();
+        }
+    }
+}
